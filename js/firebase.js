0668107e7b27a7f794e7c69c03d489ab7ff23231
Oardieldei <Oardieldei@users.noreply.github.com>
2025-10-28 15:38:14 +0000

    deploy: efe2a810029e9addf9743d102b8d81d70c6cf9b0

---
+++ js/firebase.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCStY_szTbzOfvlYnq32tqmgPAv-aRHPcQ",
+  authDomain: "tarot-layouts.firebaseapp.com",
+  projectId: "tarot-layouts",
+  storageBucket: "tarot-layouts.firebasestorage.app",
+  messagingSenderId: "698149348927",
+  appId: "1:698149348927:web:7b33f5a55dda914ab6fa8a",
+}
